207df97d11f3a2cbd7af8b44f55ab79c9648d4d7
Isaac <fimbresisaac23@gmail.com>
2022-12-27 21:12:12 -0800

    infering output type of an endpoint

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,11 @@
 import * as trpcNext from "@trpc/server/adapters/next"
-import { appRouter } from "@/backend/router"
+import { AppRouter, appRouter } from "@/backend/router"
+import type { inferRouterOutputs } from '@trpc/server';
 
 export default trpcNext.createNextApiHandler({
     router: appRouter,
     createContext: () => ({}),
 });
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
+export type GetPokemonByIdOutput = RouterOutput['getPokemonById'];
