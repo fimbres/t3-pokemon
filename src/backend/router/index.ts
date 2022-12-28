721a8cfac03b185809d67434441554b1838e1b02
Isaac <fimbresisaac23@gmail.com>
2022-12-27 20:21:51 -0800

    adding new end point for pokemons

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,21 +1,19 @@
 import { initTRPC } from '@trpc/server';
+import { PokemonClient } from 'pokenode-ts';
 import { z } from "zod";
 
 const trpc = initTRPC.create();
 
 export const appRouter = trpc.router({
-    hello: trpc.procedure
-        .input(
-            z.object({
-                text: z.string(),
-            }),
-        )
-        .query(({ input }) => {
-            return {
-                greeting: `hello ${input.text}`,
-            };
-        }
-    ),
+    getPokemonById: trpc.procedure.input(z.object({
+        id: z.number(),
+    }))
+    .query(async request => {
+        const api = new PokemonClient();
+        const pokemon = await api.getPokemonById(request.input.id);
+              
+        return pokemon;
+    })
 });
 
 export type AppRouter = typeof appRouter;
